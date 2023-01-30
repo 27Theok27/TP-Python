e00feab0b0d1590189124d21a384840b5697b0c3
Theok27 <theo.kacel@edu.itescia.fr>
2023-01-30 16:31:49 +0100

    Modification Théo

--- ScriptHumain.py
+++ ScriptHumain.py
@@ -1,1 +1,17 @@
-"" 
+class humain():
+    def __init__(self, nom, prenom):
+        self.nom = nom
+        self.prenom = prenom
+        
+    
+    def demander_Ordres(self):
+        ordre = input("Que voulez-vous faire ?")
+        return ordre
+
+    def marcher(self, x, y):
+        self.x=int(x)
+        self.y=int(y)
+        
+        print("Vous avez demandé de ", ordre)
+
+    def 
