bded77a06d27b7a2b7d6e56834fe663861bd7dc0
Leonardo Lopes <76175211+lopes1008@users.noreply.github.com>
2023-01-30 16:18:58 +0100

    Modif leo

--- ScriptRobot.py
+++ ScriptRobot.py
@@ -1,8 +1,14 @@
 class Robot:
-    def __init__(self, x, y, direction):
+    def __init__(self, name, color, weight, x, y, direction):
+        self.name = name
+        self.color = color
+        self.weight = weight
         self.x = x
         self.y = y
         self.direction = direction
+
+    def introduce_self(self):
+        print("My name is " + self.name)
 
     def avancer(self):
         if self.direction == "Z":
