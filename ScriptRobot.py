365b8590d2dacbc5b384d61bf1107fb65fc1c4b0
Leonardo Lopes <76175211+lopes1008@users.noreply.github.com>
2023-01-30 16:12:19 +0100

    Modif leo

--- ScriptRobot.py
+++ ScriptRobot.py
@@ -1,1 +1,17 @@
-# je test
+class Robot:
+    def __init__(self, x, y, direction):
+        self.x = x
+        self.y = y
+        self.direction = direction
+
+    def avancer(self):
+        if self.direction == "Z":
+            self.y += 1
+        elif self.direction == "S":
+            self.y -= 1
+        elif self.direction == "D":
+            self.x += 1
+        elif self.direction == "Q":
+            self.x -= 1
+
+    
