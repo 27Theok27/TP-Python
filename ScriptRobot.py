5f96d9d713603a0aff1a712df868d62a52d2b006
Leonardo Lopes <76175211+lopes1008@users.noreply.github.com>
2023-01-30 16:23:22 +0100

    Modif leo

--- ScriptRobot.py
+++ ScriptRobot.py
@@ -1,11 +1,12 @@
 class Robot:
-    def __init__(self, name, color, weight, x, y, direction):
+    def __init__(self, name, color, weight, x, y, direction, message):
         self.name = name
         self.color = color
         self.weight = weight
         self.x = x
         self.y = y
         self.direction = direction
+        self.message = message
 
     def introduce_self(self):
         print("My name is " + self.name)
@@ -20,4 +21,5 @@ class Robot:
         elif self.direction == "Q":
             self.x -= 1
 
-    
+    def communiquer(self, message):
+        print(self.name + " dit : " + message)
